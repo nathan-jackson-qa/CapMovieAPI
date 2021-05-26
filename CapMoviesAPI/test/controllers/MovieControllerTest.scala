1c30add231178a2671f4069821ea23b14eaf0c0c
“QAcpardeep” <cpardeep@qa.com>
2021-05-26 13:19:25 +0100

    cp: gg

--- CapMoviesAPI/test/controllers/MovieControllerTest.scala
+++ CapMoviesAPI/test/controllers/MovieControllerTest.scala
@@ -3,27 +3,41 @@ package controllers
 import controllers.abstractTest
 import dao.MovieDAO
 import models.Movie
+import org.mockito.Mockito._
+
 import scala.concurrent._
-import org.scalatestplus.mockito.MockitoSugar.mock
 import play.api.test._
 import reactivemongo.bson.BSONObjectID
 import play.modules.reactivemongo.ReactiveMongoApi
+
 import scala.util._
 import play.api.libs.json._
+import play.api.mvc.Result
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
 
 class MovieControllerTest extends abstractTest {
 
-  val mongo = mock[ReactiveMongoApi]
-  val ec = mock[ExecutionContext]
-  val dao = new MovieDAO()(ec, mongo)
+  val mongo = mock(classOf[ReactiveMongoApi])
+  val dao = mock(classOf[MovieDAO])
   val controller = new MovieController(Helpers.stubControllerComponents(), mongo, dao)
+  def await[T](arg: Future[T]): T = Await.result(arg, Duration.Inf)
+  val movie = Movie(Option(BSONObjectID.parse("609a678ce1a52451685d793f").get), "Gladiator", "Ridley Scott", "Russell Crowe", "R", "Action", "images/posters/gladiator.jpg")
 
-  "A MovieController" can {
 
-    "listMovies" should {
+  "MovieController" can {
+    "ListMovies" should {
       "return a list of movies" in {
-        controller.listMovies.apply(FakeRequest())
+        when(dao.list()) thenReturn(Future.successful(Seq(movie)))
+
+        val result: Result = await(controller.listMovies.apply(FakeRequest()))
+
+        result.header.status shouldBe(200)
+
       }
     }
   }
+
+
 }
--- CapMoviesAPI/test/controllers/abstractTest.scala
+++ CapMoviesAPI/test/controllers/abstractTest.scala
@@ -2,9 +2,8 @@ package controllers
 
 import org.scalatest.matchers.should
 import org.scalatest.wordspec.AnyWordSpec
-import org.scalatestplus.selenium.HtmlUnit
 
-trait abstractTest extends AnyWordSpec with should.Matchers with HtmlUnit {
+trait abstractTest extends AnyWordSpec with should.Matchers {
   org.slf4j.LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME)
     .asInstanceOf[ch.qos.logback.classic.Logger]
     .setLevel(ch.qos.logback.classic.Level.ERROR)
